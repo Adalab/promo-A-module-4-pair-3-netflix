e6a92fb045cc56ea415920e9a843219d6f19e86c
Sara Sanchez Crespo <saritasc89@gmail.com>
2024-03-09 11:06:00 +0100

    4.5 create  endpoint

--- src/index.js
+++ src/index.js
@@ -6,8 +6,25 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
+// Endpoint para obtener el listado de películas
+server.get('/movies', async (req, res) => {
+  try {
+    const movies = await movies.find();
+    res.json(movies);
+  } catch (error) {
+    console.error('Error al obtener las películas:', error);
+    res.status(500).json({ error: 'Error al obtener las películas' });
+  }
+});
+
 // init express aplication
 const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
+
+
+
+
+
+
